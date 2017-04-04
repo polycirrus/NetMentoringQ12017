b7c60ce7c53e38172874d343a0998b566e696499
Aliaksandr Zhytnitski <Aliaksandr_Zhytnitski@epam.com>
2017-04-04 16:56:44 +0300

    Add Expression Tree task template

---
+++ IQueryable/Samples/ClassMapper/ClassMapperTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ClassMapper
+{
+    public class Foo { }
+    public class Bar { }
+
+    [TestClass]
+    public class ClassMapperTests
+    {
+        [TestMethod]
+        public void TestMethod3()
+        {
+            var mapGenerator = new MappingGenerator();
+            var mapper = mapGenerator.Generate<Foo, Bar>();
+
+            var res = mapper.Map(new Foo());
+        }
+    }
+}
