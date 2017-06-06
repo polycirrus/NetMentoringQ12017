8687e51dace3fb3052164fdc8b2dc72c6adc1b45
Aliaksandr Zhytnitski <Aliaksandr_Zhytnitski@epam.com>
2017-06-06 19:47:46 +0300

    Implement Debug task

---
+++ DebugTask/KeyGen/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace KeyGen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.keyTextBox = new System.Windows.Forms.TextBox();
+            this.generateButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // keyTextBox
+            // 
+            this.keyTextBox.Location = new System.Drawing.Point(12, 12);
+            this.keyTextBox.Name = "keyTextBox";
+            this.keyTextBox.ReadOnly = true;
+            this.keyTextBox.Size = new System.Drawing.Size(209, 20);
+            this.keyTextBox.TabIndex = 0;
+            // 
+            // generateButton
+            // 
+            this.generateButton.Location = new System.Drawing.Point(12, 38);
+            this.generateButton.Name = "generateButton";
+            this.generateButton.Size = new System.Drawing.Size(209, 23);
+            this.generateButton.TabIndex = 1;
+            this.generateButton.Text = "Generate";
+            this.generateButton.UseVisualStyleBackColor = true;
+            this.generateButton.Click += new System.EventHandler(this.generateButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(233, 73);
+            this.Controls.Add(this.generateButton);
+            this.Controls.Add(this.keyTextBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "KeyGen";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox keyTextBox;
+        private System.Windows.Forms.Button generateButton;
+    }
+}
+
---
+++ DebugTask/KeyGen/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KeyGen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void generateButton_Click(object sender, EventArgs e)
+        {
+            var networkInterface = NetworkInterface.GetAllNetworkInterfaces().First();
+            var addressBytes = networkInterface.GetPhysicalAddress().GetAddressBytes();
+
+            var dateBytes = BitConverter.GetBytes(DateTime.Now.Date.ToBinary());
+
+            var keyNumbers = addressBytes
+                .Select((@byte, index) => @byte ^ dateBytes[index])
+                .Select(number => number <= 999 ? number * 10 : number);
+
+            var key = string.Join("-", keyNumbers.Select(number => number.ToString()));
+            keyTextBox.Text = key;
+        }
+    }
+}
